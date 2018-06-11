def56b65a1fed2dc695dc41c4bb65bdb801fe55d
invincibel <31722976+invincibel@users.noreply.github.com>
2018-06-11 22:24:53 +0530

    insertion
    add after

---
+++ circularLinkedList/3.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+ int data;
+ struct node* next;
+};
+struct node* push(struct node* last,int num)
+{
+  if(last==NULL)
+        return addToBegin(last,num);
+
+  struct node* temp = (struct node*)malloc(sizeof(struct node));
+  temp->data = num;
+  temp->next = last->next;
+  last->next = temp;
+  return last;
+}
+void addToBegin(struct node* last, int num)
+{
+  if(last!=NULL)
+    return;
+  struct node* temp = (struct node*)malloc(sizeof(struct node));
+  temp->data = num;
+  last = temp;
+  temp->next = last;
+}
+int main()
+{
+    struct node* last = NULL;
+    last = push(last,4);
+    last = push(last,3);
+    show(last);
+}
+void show(struct node* last)
+{
+    if(last==NULL)
+        printf("empty");
+  struct node* p;
+  p = last->next;
+  do{
+    printf(" %d ",p->data);
+    p = p->next;
+  }while(p != last->next);
+}
